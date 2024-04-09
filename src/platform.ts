782560898d7157d9a257d12b0d64355b3574bb6e
rajivsarvepalli <rajiv@sarvepalli.net>
2024-04-08 20:45:42 -0400

    Update error message

--- src/platform.ts
+++ src/platform.ts
@@ -129,6 +129,8 @@ export class HomebridgeMoonrakerPlatform implements DynamicPlatformPlugin {
       } else {
         this.log.error(`Unexpected error occured. See error: ${JSON.stringify(e)}`);
       }
+
+      this.log.debug('Unexpected error occured. Error: %O', e);
     }
   }
 }
